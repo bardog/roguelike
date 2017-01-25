6b15e265fbc930a7851e0e691ddca03557039429
Clive Tyrannosaurus <bmikel@TradeMe.local>
2017-01-25 13:59:35 +1300

    Uploading primary files

---
+++ assets/game.js
@@ -0,0 +1,6 @@
+// Check if rot.js can work on this browser
+if (!ROT.isSupported()) {
+    alert("The rot.js library isn't supported by your browser.");
+} else {
+    alert("The rot.js library is supported by your browser.");
+}
