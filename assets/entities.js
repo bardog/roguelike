f16bfc01c6af3da50666e933e7421a1bd10503cb
Clive Tyrannosaurus <bmikel@TradeMe.local>
2017-02-01 11:49:07 +1300

    Adding speed queue & following monsters

--- assets/entities.js
+++ assets/entities.js
@@ -4,7 +4,7 @@ Game.PlayerTemplate = {
     foreground: 'white',
     maxHp: 40,
     attackValue: 10,
-    sightRadius: 10,
+    sightRadius: 7,
     inventorySlots: 22,
     mixins: [Game.EntityMixins.PlayerActor,
              Game.EntityMixins.Attacker, Game.EntityMixins.Destructible,
@@ -18,6 +18,7 @@ Game.EntityRepository = new Game.Repository('entities', Game.Entity);
 Game.EntityRepository.define('fungus', {
     name: 'fungus',
     character: 'F',
+    speed: 250, // out of 1000
     foreground: 'green',
     maxHp: 10,
     mixins: [Game.EntityMixins.FungusActor, Game.EntityMixins.Destructible]
@@ -27,9 +28,10 @@ Game.EntityRepository.define('bat', {
     name: 'bat',
     character: 'B',
     foreground: 'white',
+    speed: 2000, // double normal speed
     maxHp: 5,
     attackValue: 4,
-    mixins: [Game.EntityMixins.WanderActor,
+    mixins: [Game.EntityMixins.TaskActor,
              Game.EntityMixins.Attacker, Game.EntityMixins.Destructible,
              Game.EntityMixins.CorpseDropper]
 });
@@ -40,7 +42,20 @@ Game.EntityRepository.define('newt', {
     foreground: 'yellow',
     maxHp: 3,
     attackValue: 2,
-    mixins: [Game.EntityMixins.WanderActor,
+    mixins: [Game.EntityMixins.TaskActor,
              Game.EntityMixins.Attacker, Game.EntityMixins.Destructible,
              Game.EntityMixins.CorpseDropper]
 });
+
+Game.EntityRepository.define('kobold', {
+  name: 'kobold',
+  character: 'k',
+  foreground: 'white',
+  maxHp: 6,
+  attackValue: 4,
+  sightRadius: 10,
+  tasks: ['hunt', 'wander'],
+  mixins: [Game.EntityMixins.TaskActor, Game.EntityMixins.Sight,
+           Game.EntityMixins.Attacker, Game.EntityMixins.Destructible,
+           Game.EntityMixins.CorpseDropper]
+})
